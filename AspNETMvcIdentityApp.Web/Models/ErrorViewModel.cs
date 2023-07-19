29f73e8419815bc3e93c024355cece19e2dc9f40
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-19 14:00:52 +0300

    Add project files.

---
+++ AspNETMvcIdentityApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNETMvcIdentityApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
