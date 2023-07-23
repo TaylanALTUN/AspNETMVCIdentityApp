f44edc3396c9539ed11329d5a56357d012445ed6
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-23 10:22:27 +0300

    Add: StartupExtensions and implement password validation

--- AspNETMvcIdentityApp.Web/Program.cs
+++ AspNETMvcIdentityApp.Web/Program.cs
@@ -11,7 +11,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
-builder.Services.AddIdentity< AppUser,AppRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<AppUser, AppRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz1234567890_";
+
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+}).AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
